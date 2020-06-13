8d1edeef284ed0fe653f1843df12aa81e9af6d1c
chickendude <pokemaster103@gmail.com>
2020-06-13 19:45:31 +0200

    Create party member resources for Yuji and Ximi and prepare Player singleton to support party.

--- Singletons/Player.gd
+++ Singletons/Player.gd
@@ -3,6 +3,59 @@ extends Node
 var position = Vector2(19*16 - 8, 3*16 - 8)
 var direction = Vector2.DOWN
 
+var Members = {
+	'Yuji': preload("res://Fighters/Party/Yuji.tres"),
+	'Ximi': preload("res://Fighters/Party/Ximi.tres")
+}
+
+
+var party := []
+var weapons := []
+var armor := []
+var items := {}
+
+var can_move := true
+
+func _ready():
+	# starting party
+	party.append(Members.Yuji)
+	party.append(Members.Ximi)
+	
+	# starting equipment
+#	armor.append(starting_armor)
+	armor.append(Equipment.ximis_coat)
+#	weapons.append(starting_weapon)
+	weapons.append(Equipment.pocket_knife)
+	weapons.append(Equipment.dagger)
+	weapons.append(Equipment.short_sword)
+#	equipped_weapon = starting_weapon
+	receive_items('tissue', 3)
+	receive_items('handkerchief', 1)
+
+func receive_items(item_name : String, amt = 0) -> void:
+	print("Received " + str(amt) + " " + item_name + "(s)")
+	if items.has(item_name):
+		items[item_name] = min(items[item_name] + amt, 99)
+	else:
+		items[item_name] = amt
+
+func use_item(item_name : String):
+	if items.has(item_name):
+		items[item_name] -= 1
+		if items[item_name] <= 0:
+			items.erase(item_name)
+		var item = Items.all[item_name]
+		if item.permanent:
+			max_hp += item.hp
+			max_mp += item.mp
+		else:
+			self.hp += item.hp
+			mp += item.mp
+
+
+
+
+
 const Combos = [
 	{'moves': "UDU", 'name': "Clockwheel", 'power': 1.2, 'num_hits': 2},
 	{'moves': "LRU", 'name': "Slobdash", 'power': 1.2, 'num_hits': 2},
@@ -11,8 +64,6 @@ const Combos = [
 	{'moves': "LLUU", 'name': "Floatlag", 'power': 1.3, 'num_hits': 2},
 	{'moves': "LRLLD", 'name': "Sinking Sill", 'power': 1.2, 'num_hits': 3},
 ]
-
-
 
 var _name = "Yuji"
 
@@ -36,31 +87,17 @@ var speed = 12
 var starting_armor = Equipment.t_shirt
 var starting_weapon = Equipment.fist
 
-var weapons = []
-var armor = []
+
 var combos_learned = []
-var items = {}
 var equipped_weapon = null
 var equipped_armor = null
 
 # movement
-var can_move = true
 
 signal player_dead
 signal level_up
 signal hp_changed
 
-func _ready():
-	armor.append(starting_armor)
-	armor.append(Equipment.ximis_coat)
-	equipped_armor = starting_armor
-	weapons.append(starting_weapon)
-	weapons.append(Equipment.pocket_knife)
-	weapons.append(Equipment.dagger)
-	weapons.append(Equipment.short_sword)
-	equipped_weapon = starting_weapon
-	receive_items('tissue', 3)
-	receive_items('handkerchief', 1)
 
 func _set_hp(_hp):
 	hp = clamp(_hp, 0, max_hp)
@@ -94,26 +131,6 @@ func _next_level_xp() -> int:
 	var part2 = lvl * lvl * .8
 	var part3 = lvl * 10
 	return int(part1 + part2 + part3)
-
-func receive_items(item_name : String, amt = 0) -> void:
-	print("Received " + str(amt) + " " + item_name + "(s)")
-	if items.has(item_name):
-		items[item_name] = min(items[item_name] + amt, 99)
-	else:
-		items[item_name] = amt
-
-func use_item(item_name : String):
-	if items.has(item_name):
-		items[item_name] -= 1
-		if items[item_name] <= 0:
-			items.erase(item_name)
-		var item = Items.all[item_name]
-		if item.permanent:
-			max_hp += item.hp
-			max_mp += item.mp
-		else:
-			self.hp += item.hp
-			mp += item.mp
 
 func xp_until_next_level() -> int:
 	return _next_level_xp() - xp
