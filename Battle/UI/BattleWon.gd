cc542c64ca1dce04008e53e971d82b0de7583b40
chickendude <pokemaster103@gmail.com>
2020-07-05 18:03:02 +0200

    Add remaining XP, animate total XP, and allow user to skip XP countdown animation

--- Battle/UI/BattleWon.gd
+++ Battle/UI/BattleWon.gd
@@ -6,16 +6,15 @@ onready var vbox_container = $NinePatchRect/VBoxContainer
 onready var xp_label = $NinePatchRect/VBoxContainer/HBoxContainer/XPLabel
 
 const CharacterStats = preload("res://Battle/UI/CharacterStats.tscn")
+const XP_TIME = 2
 
 enum {
 	WAITING, ADDING_XP, DONE_ADDING_XP
 }
 
 var xp : int
-var adding_xp := false
-var added_xp := false
 var all_character_stats = []
-var state = WAITING
+var state : int
 
 func input(_delta):
 	accept_event()
@@ -26,13 +25,15 @@ func input(_delta):
 				_add_xp()
 		ADDING_XP:
 			if accept_pressed:
-				tween.seek(2)
+				tween.stop_all()
+				_update_xp_complete()
 		DONE_ADDING_XP:
 			if Input.is_action_just_pressed("ui_cancel") or accept_pressed:
 				Event.end_battle()
 	
 
 func load_prizes(_xp : int, sils : int) -> void:
+	state = WAITING
 	xp = _xp
 	xp_label.text = str(xp)
 	sil_label.text = str(sils)
@@ -48,18 +49,19 @@ func _load_character_stats() -> void:
 
 func _add_xp():
 	state = ADDING_XP
-	tween.interpolate_method(self, '_update_xp', 0, xp, 2, Tween.TRANS_CUBIC, Tween.EASE_IN_OUT)
+	tween.interpolate_method(self, '_update_xp', 0, xp, XP_TIME, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
 	tween.start()
 	tween.connect("tween_completed", self, '_update_xp_complete')
 
 func _update_xp(_xp):
+	xp_label.text = str(int(xp - _xp))
 	for character_stats in all_character_stats:
 		if character_stats.character.alive:
 			character_stats.update_xp(_xp)
 
-func _update_xp_complete(_obj, _key):
+func _update_xp_complete(_obj = null, _key = null):
 	state = DONE_ADDING_XP
-#	for character_stats in all_character_stats:
-#		if character_stats.character.alive:
-#			character_stats.update_xp(_xp)
-#			character.xp += total_xp - added_xp
+	xp_label.text = '0'
+	for character_stats in all_character_stats:
+		if character_stats.character.alive:
+			character_stats.add_all_xp(xp)
--- Battle/UI/CharacterStats.gd
+++ Battle/UI/CharacterStats.gd
@@ -11,6 +11,7 @@ onready var next_level = $NextLevelLabel
 
 var character : PartyMember
 var added_xp : int
+var starting_level : int
 
 func _ready():
 	character_name.text = character.name
@@ -21,15 +22,22 @@ func _ready():
 
 func load_character(_character : PartyMember) -> void:
 	character = _character
+	starting_level = character.level
 
 func update_xp(_xp):
 	var next_lev = character.xp_until_next_level()
-	var xp = character.xp_until_next_level() - (_xp - added_xp)
-	xp = max(xp, 0)
-	xp_left.text = str(int(xp))
+	var next_lev_xp = character.xp_until_next_level() - (_xp - added_xp)
+	next_lev_xp = max(next_lev_xp, 0)
+	xp_left.text = str(int(next_lev_xp))
 
-	if xp == 0:
+	if next_lev_xp == 0:
 		character.xp += _xp - added_xp
 		added_xp = _xp
 		next_level.text = str(character.level + 1)
 		level_up_label.show()
+
+func add_all_xp(xp):
+	character.xp += xp - added_xp
+	xp_left.text = str(character.xp_until_next_level())
+	next_level.text = str(character.level + 1)
+	level_up_label.visible = starting_level != character.level
